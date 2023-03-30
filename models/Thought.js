23910e8a5207de7ebb08e5123b4e45ff40d11096
Naod Hunde <chris_naod@yahoo.com>
2023-03-30 14:05:02 +1100

    model for thought

---
+++ models/Thought.js
@@ -0,0 +1,77 @@
+// Import the required Mongoose modules and Moment.js for date formatting
+const { Schema, model } = require("mongoose");
+const moment = require("moment");
+
+// Define the Reaction schema
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Schema.Types.ObjectId(), // Generate a new ObjectId by default
+    },
+    reactionBody: {
+      type: String,
+      required: true, // Reaction body is required
+      maxLength: 280, // Reaction body has a maximum length of 280 characters
+    },
+    username: {
+      type: String,
+      required: true, // Username is required
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now, // Set the current date and time by default
+      get: (timestamp) => moment(timestamp).format("MMM DD, YYYY [at] hh:mm a"), // Format the createdAt timestamp
+    },
+  },
+  {
+    toJSON: {
+      getters: true, // Enable getters for JSON serialization
+    },
+    id: false,
+  }
+);
+
+// Define the Thought schema
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true, // Thought text is required
+      minLength: 1, // Thought text has a minimum length of 1 character
+      maxLength: 280, // Thought text has a maximum length of 280 characters
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now, // Set the current date and time by default
+      get: (timestamp) => moment(timestamp).format("MMM DD, YYYY [at] hh:mm a"), // Format the createdAt timestamp
+    },
+    username: {
+      type: String,
+      required: true, // Username is required
+    },
+    reactions: [ReactionSchema], // Embed the Reaction schema as an array
+  },
+  {
+    toJSON: {
+      virtuals: true, // Enable virtual properties for JSON serialization
+      getters: true, // Enable getters for JSON serialization
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property to count the number of reactions
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length; // Return the length of the reactions array
+});
+
+// Configure the schema to include virtual properties when calling toObject() and toJSON()
+ThoughtSchema.set("toObject", { virtuals: true });
+ThoughtSchema.set("toJSON", { virtuals: true });
+
+// Create the Thought model based on the Thought schema
+const Thought = model("Thought", ThoughtSchema);
+
+// Export the Thought model
+module.exports = Thought;
