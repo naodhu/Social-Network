38fe3c2151d2507efe058805f9fcf05f2d8ad4be
Naod Hunde <chris_naod@yahoo.com>
2023-03-30 14:09:46 +1100

    create and delete reaction function

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -72,3 +72,38 @@ const thoughtController = {
       })
       .catch((err) => res.status(400).json(err));
   },
+
+  createReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought found with this id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  deleteReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought found with this id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+};
+
+module.exports = thoughtController;
