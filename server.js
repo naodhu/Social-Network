d76aa9c8796c59b1815ac4bd6efd9dfd89a4cb5e
Naod Hunde <chris_naod@yahoo.com>
2023-04-02 22:20:32 +1000

    added comments for server side code

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes");
-const db = require("./config/connection");
+const express = require("express"); // Import the express module
+const mongoose = require("mongoose"); // Import the mongoose module
+const routes = require("./routes"); // Import the routes module
+const db = require("./config/connection"); // Import the connection module
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const app = express(); // Create an express server
+const PORT = process.env.PORT || 3001; // Define the port
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(require("./routes"));
+app.use(express.json()); // Parse incoming JSON data
+app.use(express.urlencoded({ extended: true })); // Parse incoming URL-encoded data
+app.use(require("./routes")); // Use the imported routes
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/noSQL-project",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect( // Connect to the MongoDB database
+  process.env.MONGODB_URI || "mongodb://localhost:27017/noSQL-project", 
+  { 
+    useNewUrlParser: true, // Use the new URL parser
+    useUnifiedTopology: true, // Use the new server discovery and monitoring engine
   }
 );
 
 // Use this to log mongo queries being executed!
 mongoose.set("debug", true);
 
+app.use(routes); // Use the imported routes
 app.listen(PORT, () => console.log(`Connected on localhost: ${PORT}`));
