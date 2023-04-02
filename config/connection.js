13a4b20c0a863d0674bbfc5b32f98620d3f9f847
Naod Hunde <chris_naod@yahoo.com>
2023-04-02 22:21:11 +1000

    added comment to connection.js to explain why we are using the   "connection" event instead of the "connect" event.

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,28 @@
 // social-network-NoSQL/config/connection.js
 
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose"); // Import the Mongoose module
+const dotenv = require("dotenv"); // Import the dotenv module
 
-dotenv.config();
+dotenv.config(); // Configure dotenv
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network';
+// Define the MongoDB connection string
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network";
 
+// Define the connectDB function
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
+      useNewUrlParser: true, // Use the new URL parser
+      useUnifiedTopology: true, // Use the new server discovery and monitoring engine
+      useCreateIndex: true, // Use the new createIndex() function instead of ensureIndex()
+      useFindAndModify: false, // Use the new findOneAndUpdate() and findOneAndDelete() functions instead of findAndModify()
     });
 
-    console.log('MongoDB connected successfully.');
+    console.log("MongoDB connected successfully."); // Log a success message to the console
   } catch (error) {
-    console.error(`Error connecting to MongoDB: ${error.message}`);
+    // Catch any errors
+    console.error(`Error connecting to MongoDB: ${error.message}`); // Log the error message to the console
     // Exit process with failure
     process.exit(1);
   }
