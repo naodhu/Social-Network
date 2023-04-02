d2c5da5ffcf34ec111eba36d63e9a3aeb568cc75
Naod Hunde <chris_naod@yahoo.com>
2023-04-02 11:28:04 +1000

    final commit

--- models/Thought.js
+++ models/Thought.js
@@ -3,36 +3,6 @@ const { Schema, model } = require("mongoose");
 const moment = require("moment");
 const ReactionSchema = require("./Reaction");
 
-// Define the Reaction schema
-// const ReactionSchema = new Schema(
-//   {
-//     reactionId: {
-//       type: Schema.Types.ObjectId,
-//       default: () => new Schema.Types.ObjectId(), // Generate a new ObjectId by default
-//     },
-//     reactionBody: {
-//       type: String,
-//       required: true, // Reaction body is required
-//       maxLength: 280, // Reaction body has a maximum length of 280 characters
-//     },
-//     username: {
-//       type: String,
-//       required: true, // Username is required
-//     },
-//     createdAt: {
-//       type: Date,
-//       default: Date.now, // Set the current date and time by default
-//       get: (timestamp) => moment(timestamp).format("MMM DD, YYYY [at] hh:mm a"), // Format the createdAt timestamp
-//     },
-//   },
-//   {
-//     toJSON: {
-//       getters: true, // Enable getters for JSON serialization
-//     },
-//     id: false,
-//   }
-// );
-
 // Define the Thought schema
 const ThoughtSchema = new Schema(
   {
